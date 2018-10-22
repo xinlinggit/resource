e8c2460398e68793cc60fefbd6f387b8dc8c76a5
xinling <851828704@qq.com>
2018-10-22 16:02:06 +0800

    更新

--- php事务.php
+++ php事务.php
@@ -5,7 +5,7 @@ $m=D('YourModel');//或者是M();
 $m2=D('YouModel2');
 $m->startTrans();//在第一个模型里启用就可以了，或者第二个也行
 $result=$m->where('删除条件')->delete();
-$result2=m2->where('删除条件')->delete();
+$result2=$m2->where('删除条件')->delete();
 if($result && $result2){
 $m->commit();//成功则提交
 }else{
